41c999bfc866f78ba8c6869119f165750e7ead19
Bedashree Aparna Sahoo <aparna.bedashree@gmail.com>
2023-11-28 20:20:12 +0530

    Created components for login-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './login/register/register.component';
+import { LoginComponent } from './login/login/login.component';
+import { ForgotPasswordComponent } from './login-module/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './login-module/reset-password/reset-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    ForgotPasswordComponent,
+    ResetPasswordComponent
   ],
   imports: [
     BrowserModule,
