e5290417e8dff9c23b7efb67362058f165f750e1
Bedashree Aparna Sahoo <aparna.bedashree@gmail.com>
2023-11-29 13:52:17 +0530

    the UI of Login module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './login-module/register/register.component';
+import { LoginComponent } from './login-module/login/login.component';
+import { ForgotPasswordComponent } from './login-module/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './login-module/reset-password/reset-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+{path: 'register', component: RegisterComponent},
+{path: 'forgot-password', component: ForgotPasswordComponent},
+{path: 'reset-password', component: ResetPasswordComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './login/register/register.component';
-import { LoginComponent } from './login/login/login.component';
+import { RegisterComponent } from './login-module/register/register.component';
+import { LoginComponent } from './login-module/login/login.component';
 import { ForgotPasswordComponent } from './login-module/forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './login-module/reset-password/reset-password.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +26,14 @@ import { ResetPasswordComponent } from './login-module/reset-password/reset-pass
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
